4b39eb49cc7ae2ab4b0ab18a23cebe4c8b229426
alpkarli <surqurer@gmail.com>
2024-06-10 23:11:47 +0300

    changed isrequired

--- src/component/ColorInverter/ColorInverter.component.js
+++ src/component/ColorInverter/ColorInverter.component.js
@@ -15,7 +15,7 @@ export class ColorInverterComponent extends PureComponent {
         isDarkModeEnabled: PropTypes.bool.isRequired,
         // a special React prop that receives the children elements
         // eslint-disable-next-line react/boolean-prop-naming
-        children: PropTypes.bool.isRequired,
+        children: PropTypes.func.isRequired,
     };
 
     render() {
